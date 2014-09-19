0b6c598040782d5bf2eb109ea07d557ee3125ad2
faust93 <monumentum@gmail.com>
2014-09-19 17:48:00 +0400

    slim cpu governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -386,6 +386,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLUACTIVE)
 extern struct cpufreq_governor cpufreq_gov_bluactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_bluactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
+extern struct cpufreq_governor cpufreq_gov_slim;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_slim)
 #endif
 
 
